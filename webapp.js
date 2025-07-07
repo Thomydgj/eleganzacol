2cded2113176c207a1470b9eee7fefa70c03849b
Thomas DG Jaimes <thomydgj@gmail.com>
2025-07-07 00:23:25 -0500

    imagen final y animacion creadas, link a coleccion

--- webapp.js
+++ webapp.js
@@ -1,5 +1,5 @@
 document.getElementById('formulario').addEventListener('submit', async (e) => {
-
+  e.preventDefault();
   const formData = new FormData(e.target);
 
   fetch('https://script.google.com/macros/s/AKfycby32ntqQnotOcZnvKNjCXAC5k84leLPqh49ZqDDubX2TFxPlUC8Ic2O4e2X9Hl3Lgz9/exec', {
@@ -8,6 +8,46 @@ document.getElementById('formulario').addEventListener('submit', async (e) => {
     body: formData
   });
 
-  alert('✅ ¡Datos enviados con éxito!');
-  reiniciarFormulario(); // 👈 Limpia el formulario para el siguiente usuario
+  // Desplaza la vista al tope
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+
+  // Muestra la imagen después de desplazarse
+  setTimeout(() => {
+    document.getElementById('oferta').style.display = 'block';
+    document.body.style.overflow = 'hidden'; // Evitar scroll al mostrar la imagen
+  }, 300); // espera breve para que el scroll se complete visualmente
+ 
+  document.querySelectorAll('.hero-image,.colaboracion').forEach(element => {
+    element.style.opacity = '0.3';
+  });
+
+  // Reiniciar el formulario después de enviar
+  reiniciarFormulario();
+
+  // Esperar un poco antes de permitir el cierre al hacer clic fuera
+  setTimeout(() => {
+    document.addEventListener('click', cerrarAlClickFuera);
+  }, 50);
 });
+
+
+// Función para reiniciar el formulario
+function reiniciarFormulario() {
+  document.getElementById('formulario').reset();
+ }
+
+// Función para cerrar la imagen al hacer clic fuera de ella
+function cerrarAlClickFuera(e) {
+  const imagen = document.getElementById('oferta');
+
+  // Si se hace clic fuera de la imagen
+  if (!imagen.contains(e.target)) {
+    imagen.style.display = 'none';
+    document.removeEventListener('click', cerrarAlClickFuera);
+    document.body.style.overflow = 'auto'; // Restaurar scroll
+    document.querySelectorAll('.hero-image,.colaboracion').forEach(element => {
+      element.style.opacity = '1';
+    });
+
+  }
+}
